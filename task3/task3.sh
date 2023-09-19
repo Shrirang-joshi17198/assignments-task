185c3910e3ae35b6106e24e74f1ab43262cce72c
Shrirang-joshi17198 <shrirangjoshisj@gmail.com>
2023-09-19 18:43:37 +0000

    task3

---
+++ task3/task3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir mydirectory
+cd mydirectory
+touch myfile.txt
+echo "Hello World" > myfile.txt
+
+echo "---------------------------------------------"
+
+echo "Current date and time: $(date)"
+
+echo "--------------------------------------------"
